ea786d6569eb39ddedcfb4be666d8089aba36c36
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-02-13 17:56:46 -0400

    Images setup change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    image: {
+        deviceSizes: [375, 640, 768, 1024, 1280, 1440, 1920],
+        imageSizes: [64, 128, 192],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
